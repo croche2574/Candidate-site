5c2228858608e1139f96d69959e7cdacffa1e3aa
croche2574 <croche2574@flpoly.org>
2018-04-21 16:29:11 -0400

    Added a helper to check if there are responses, and display the correct message.  Removed info column from db

--- app/helpers/candidates_helper.rb
+++ app/helpers/candidates_helper.rb
@@ -8,4 +8,22 @@ module CandidatesHelper
     imgur_url = "https://imgur.com/#{candidate.imgur_id}.jpg"
     image_tag(imgur_url, alt: candidate.name, class: "imgur")
   end 
+
+  def infoText(candidate)
+    noResponse = true;
+    candidate.attributes.each do |key, value|
+      if key.to_s.include?("section")
+        if value.present?
+          noResponse = false         
+        end
+      end
+    end
+    
+    if noResponse == false
+      "In addition to answering the 20 survey questions, this candidate provided the following elaboration on several progressive issues."
+    else
+      "Beyond answering “Yes” or “No” to the 20 survey questions, this candidate did not provide elaboration on progressive issues."
+    end
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180403232624) do
+ActiveRecord::Schema.define(version: 2018_04_03_232624) do
 
   create_table "candidates", force: :cascade do |t|
     t.string "race"
